d141682a8cb713d5f392b033cc875695fd9c4b9f
Mike <ms53150@gmail.com>
2022-05-09 09:10:49 -0400

    Add function to change card after 3 seconds

--- main.py
+++ main.py
@@ -9,12 +9,23 @@ FONT_NAME = "Arial"
 # Pick random word from csv
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 def generate_word():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    front_card.itemconfig(title_text, text="French")
-    front_card.itemconfig(word_text, text=current_card["English"])
+    flash_card.itemconfig(title_text, text="French", fill="black")
+    flash_card.itemconfig(word_text, text=current_card["French"], fill="black")
+    flash_card.itemconfig(card_background, image=front_card_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    flash_card.itemconfig(title_text, text="English", fill="white")
+    flash_card.itemconfig(word_text, text=current_card["English"], fill="white")
+    flash_card.itemconfig(card_background, image=back_card_img)
 
 
 # ---- UI Setup
@@ -22,14 +33,17 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# timer
+flip_timer = window.after(3000, func=flip_card)
+
 # front card
 front_card_img = PhotoImage(file="images/card_front.png")
 back_card_img = PhotoImage(file="images/card_back.png")
-front_card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-front_card.create_image(400, 263, image=front_card_img)
-title_text = front_card.create_text(400, 150, text="", font=(FONT_NAME, 40, "italic"))
-word_text = front_card.create_text(400, 263, text="", font=(FONT_NAME, 60, "bold"))
-front_card.grid(row=0, column=0, columnspan=2)
+flash_card = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_background = flash_card.create_image(400, 263, image=front_card_img)
+title_text = flash_card.create_text(400, 150, text="", font=(FONT_NAME, 40, "italic"))
+word_text = flash_card.create_text(400, 263, text="", font=(FONT_NAME, 60, "bold"))
+flash_card.grid(row=0, column=0, columnspan=2)
 
 # Buttons
 wrong_button_img = PhotoImage(file="images/wrong.png")
